b3afe40b412d36ed7b3c793032b227d192fc35e5
Aziret <utachi86@gmail.com>
2025-11-07 23:20:07 +0300

    исправил ДЗ

--- homework_3.py
+++ homework_3.py
@@ -1,31 +1,37 @@
 class Person :
     def __init__(self, name, birth_date, occupation,  higher_education):
-            self.name = name
-            self.birth_date = birth_date
-            self.occupation = occupation
-            self.higher_education = higher_education
-            self.__occupation = occupation
-            self.__higher_education = higher_education
+             self.name = name
+             self.birth_date = birth_date
+             self.__occupation = occupation
+             self.__higher_education = higher_education
+             self.__occupation = occupation
+             self.__higher_education = higher_education
     def introduce (self):
-            print(f" Меня зовут  {self.name}. Я родился {self.birth_date}. Мой профессия{self.__occupation}.У меня нет высшее образование{self.__higher_education}")
+             print(f" Меня зовут  {self.name}. Я родился {self.birth_date}. Мой профессия{self.__occupation}.У меня нет высшее образование{self.__higher_education}")
+
+    def get_occupation(self):
+        return self.__occupation
+
+    def set_higher_education(self, value):
+         self.__higher_education = value
 
 class Classmate(Person):
     def __init__(self,name, birth_date, occupation,  higher_education, group_name ):
-        super().__init__(name, birth_date, occupation, higher_education )
-        self.group_name = group_name
-    def introduce (self):
-        super().introduce()
-        print(f"мой гуппа {self.group_name}")
+         super().__init__(name, birth_date, occupation, higher_education )
+         self.group_name = group_name
+         def introduce (self):
+          super().introduce()
+          print(f"мой гуппа {self.group_name}")
 
 
 
 class Friend(Person):
-    def __init__(self,name, birth_date, occupation, higher_education, hobby ):
-           super().__init__(name, birth_date, occupation,higher_education)
-           self.hobby = hobby
-    def introduce (self):
-           super().introduce()
-           print(f"хобби друга {self.hobby}")
+     def __init__(self,name, birth_date, occupation, higher_education, hobby ):
+            super().__init__(name, birth_date, occupation,higher_education)
+            self.hobby = hobby
+     def introduce (self):
+            super().introduce()
+            print(f"хобби друга {self.hobby}")
 
 person = Person("Азамат", ":12.05.2000", ":Ветеринар", ":Да")
 
@@ -37,4 +43,6 @@ friend = Friend("Эрмек", ":15.07.1999", ":Программист", ":Да",
 person.introduce ()
 classmate.introduce()
 friend.introduce()
+
+
 
